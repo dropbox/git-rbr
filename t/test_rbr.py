40e23b52ccdd52238deb482c6d883873cd4099de
Greg Price <gregprice@dropbox.com>
2017-02-16 15:57:41 -0800

    more tests

--- t/test_rbr.py
+++ t/test_rbr.py
@@ -3,6 +3,7 @@
 
 import os.path
 import pytest
+import re
 import shutil
 import subprocess
 from subprocess import check_output, check_call
@@ -13,12 +14,13 @@ import tempfile
 def repo():
     # type: () -> str
     d = tempfile.mkdtemp()
-    check_output(['git', 'init', d])
     git_dir = os.environ.get('GIT_DIR')
     os.environ['GIT_DIR'] = os.path.join(d, '.git')
     cwd = os.getcwd()
     os.chdir(d)
+    check_call(['git', 'init', d])
 
+    print d
     return d
 
     # Pytest 2.6.1 which I have handy doesn't support this.  Forget it for now.
@@ -34,15 +36,51 @@ def repo():
 
 def shell(cmds):
     # type: (str) -> None
-    check_output(['sh', '-c', cmds])
+    try:
+        check_output(['sh', '-exc', cmds])
+    except subprocess.CalledProcessError as e:
+        cmds_fmtd = re.sub('^', '  ', cmds.strip('\n'), flags=re.M) + '\n'
+        raise RuntimeError('Shell commands exited with code %s:\n%s'
+                           % (e.returncode, cmds_fmtd))
+
+
+def expect_conflict(cmd):
+    # type: (str) -> None
+    '''Expect `cmd` to fail with a conflict message.'''
+    try:
+        out = check_output(cmd, stderr=subprocess.STDOUT)
+        print out
+        raise RuntimeError('Expected conflict; none happened')
+    except subprocess.CalledProcessError as e:
+        if 'git rbr --continue' not in e.output:
+            print e.output
+            raise RuntimeError('Expected conflict; got different message')
 
 
 def setup_shell(cmds):
     # type: (str) -> None
     preamble = '''
-testci () { mkdir -p "$(dirname "$1")" && echo "$1" >"$1" && git add "$1" && git commit -m "$1"; }
+testci () {
+  # args: message [filename [contents]]
+  # optional args default to message
+  mkdir -p "$(dirname "${2:-$1}")" &&
+  echo "${3:-$1}" >"${2:-$1}" &&
+  git add "${2:-$1}" &&
+  git commit -m "$1"
+}
 '''
     shell(preamble + cmds)
+
+
+def subjects_between(upstream, branch):
+    return check_output(['git', 'log', '--pretty=format:%s', '--reverse',
+                         '%s..%s' % (upstream, branch)]).strip('\n').split('\n')
+
+
+def assert_atop(upstream, branch):
+    assert '0' == check_output(
+        ['git', 'rev-list', '--count', '--max-count=1',
+         upstream, '--not', branch, '--']).strip()
 
 
 def assert_updated(branches=None):
@@ -54,9 +92,7 @@ def assert_updated(branches=None):
         branches = all_branches - set(['master'])
 
     for branch in branches:
-        assert '0' == check_output(
-            ['git', 'rev-list', '--count', '--max-count=1',
-             branch+'@{u}', '--not', branch, '--']).strip()
+        assert_atop(branch+'@{u}', branch)
 
 
 def test_simple(repo):
@@ -79,3 +115,69 @@ git checkout a
 ''')
     check_call(['git', 'rbr', '-v'])
     assert_updated()
+    assert ' '.join(subjects_between('master^', 'c')) == 'master2 a a2 b b2 c'
+
+
+def test_fork(repo):
+    # master <- a <- b
+    #             <- c
+    # each advanced, no conflicts
+    setup_shell('''
+testci master
+git checkout -tb a
+testci a
+git checkout -tb b
+testci b
+git checkout a -tb c
+testci c
+git checkout master
+testci master2
+git checkout a
+testci a2
+''')
+    check_call(['git', 'rbr', '-v'])
+    assert_updated()
+
+
+@pytest.fixture
+def repo_conflicted(repo):
+    # master -> a -> b -> ab -> c
+    # master, a advanced
+    # a, ab conflict
+    setup_shell('''
+testci master
+git checkout -tb a
+testci a
+git checkout -tb b
+testci b
+git checkout -tb ab
+testci ab a
+git checkout -tb c
+testci c
+git checkout master
+testci master2
+git checkout a
+testci aa a
+''')
+
+
+def test_continue(repo_conflicted):
+    setup_shell('git checkout a')
+    expect_conflict(['git', 'rbr', '-v'])
+    check_call(['git', 'add', '-u'])
+    check_call(['git', 'rbr', '--continue'])
+    assert_updated()
+    assert ' '.join(subjects_between('master^', 'c')) == 'master2 a aa b ab c'
+
+
+def test_skip(repo_conflicted):
+    setup_shell('git checkout a')
+    expect_conflict(['git', 'rbr', '-v'])
+    check_call(['git', 'rbr', '--skip'])
+    assert_updated()
+    assert ' '.join(subjects_between('master^', 'c')) == 'master2 a aa b c'
+
+
+def test_abort(repo_conflicted):
+    setup_shell('git checkout a')
+    # TODO WORK HERE
